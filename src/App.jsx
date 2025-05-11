539c528f4f20a0ad60082fe65de66d30307c93d1
avinav baral <baralavinav2495@gmail.com>
2025-05-11 22:36:36 +0545

    Frontend (Login,signup, home page and admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "../src/Pages/Login/login";
+import Signup from "./Pages/SignUp/Signup";
+import HomePage from "../src/Pages/Design/MainHomePage";
+import AdminDashboard from "../src/Pages/Admin /AdminPage";
 function App() {
-  // const [count, setCount] = useState(0)
-
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="*" element={<Login />} /> {/* fallback to login */}
+      <Route path="/AdminDashboard" element={<AdminDashboard />} />
+      {/* Add more routes here */}
+    </Routes>
+  );
 }
 
 export default App;
